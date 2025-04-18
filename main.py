877524e7e54c09798ae5c98d898c145679737b6a
canaankip <canaankiprop@gmail.com>
2025-04-19 00:43:07 +0300

    Updated pet class and main script

---
+++ main.py
@@ -0,0 +1,26 @@
+from pet import Pet
+
+# Create a pet object
+buddy = Pet("Buddy")
+
+# Show initial status
+buddy.get_status()
+
+# Feed the pet
+buddy.eat()
+
+# Play with the pet
+buddy.play()
+
+# Put the pet to sleep
+buddy.sleep()
+
+# Teach the pet a trick
+buddy.train("roll over")
+
+# Show learned tricks
+buddy.show_tricks()
+
+# Final status
+buddy.get_status()
+
